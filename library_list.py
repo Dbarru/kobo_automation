552d99a831fe04d0bb2835c886c0badfebef4d78
Dbarru <145423077+Dbarru@users.noreply.github.com>
2025-05-21 18:18:00 +0200

    Update library_list.py
    add comment

--- library_list.py
+++ library_list.py
@@ -2,17 +2,17 @@ import pandas as pd
 import requests
 import sqlite3
 
-# Connessione al database (crea il file se non esiste)
-conn = sqlite3.connect("/home/barru/Biblioteca di calibre/metadata.db")
+#connection to the database
+conn = sqlite3.connect("your/path/to/calibre/ligtsql.db")
 
-# Creazione di un cursore per eseguire comandi SQL
+#create a cursor to execute SQL command
 cursor = conn.cursor()
 
+#select only the book's title and fetch them
 cursor.execute("SELECT title FROM books")
 books = cursor.fetchall()
 lista_libri = []
 
+#save the book's title in a list 
 for book in books:
    lista_libri.append(book[0])
-
-print(lista_libri)
